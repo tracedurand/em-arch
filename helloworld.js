18826e7695fdb37f0458a40d95f44f7f41e0e15a
Trace Durand <tdurand@tdurand-ltm3.internal.salesforce.com>
2018-09-09 09:12:00 -0600

    update hello world.

--- helloworld.js
+++ helloworld.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(__filename + '<br>');
     res.write(__dirname + '<br>');
-    res.end('Hello World!!!');
+    res.end('Hello World!!!!');
 }).listen(8080);
